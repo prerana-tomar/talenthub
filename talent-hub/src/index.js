28f458e6aeddd9f9077e890b5856d2e3be21e2bf
prerana-tomar <p89148635@gmail.com>
2026-06-16 16:13:13 +0530

    fix: wake up server on app load

--- talent-hub/src/index.js
+++ talent-hub/src/index.js
@@ -4,6 +4,9 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 import App from './App';
 
+// Server ko app load hote hi wake up karo
+fetch('https://talenthub-w1cc.onrender.com/api/auth/count').catch(() => {});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <GoogleOAuthProvider clientId="171142887054-79f8f7jusmm4cmaevce5p8gv6c5g82o0.apps.googleusercontent.com">
